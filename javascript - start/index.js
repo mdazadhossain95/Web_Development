5d33ad58a640725d2d6ee6abd9ae3baeaac9f1e4
mdazadhossain95 <mdazadhossain95@gmail.com>
2021-06-29 14:09:18 +0600

    # create folders Bootstrap, DOM folder, javascript, TimDog-start-master, NodeJs

---
+++ javascript - start/index.js
@@ -0,0 +1,15 @@
+var name = prompt("what is your name:");
+
+var firstChar = name.slice(0, 1);
+
+var UpperCaseOfName =  firstChar.toUpperCase();
+
+var restOfName = name.slice(1, name.length);
+
+restOfName = restOfName.toLowerCase();
+
+var capitalisedName = UpperCaseOfName + restOfName;
+
+alert("Hello " + capitalisedName);
+
+
